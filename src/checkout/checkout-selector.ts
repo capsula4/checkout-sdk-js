4adefd9cfae0808f19cedbb37d7c1b9a2bdb4552
Luis Sanchez <luis.sanchez@bigcommerce.com>
2019-07-29 15:55:51 +1000

    feat(payment): CHECKOUT-4263 Support for applying/removing store credit

--- src/checkout/checkout-selector.ts
+++ src/checkout/checkout-selector.ts
@@ -11,7 +11,7 @@ import CheckoutState, { DEFAULT_STATE } from './checkout-state';
 
 export default interface CheckoutSelector {
     getCheckout(): Checkout | undefined;
-    getGrandTotal(useStoreCredit?: boolean): number | undefined;
+    getOutstandingBalance(useStoreCredit?: boolean): number | undefined;
     getLoadError(): Error | undefined;
     isLoading(): boolean;
     getUpdateError(): Error | undefined;
@@ -70,7 +70,7 @@ export function createCheckoutSelectorFactory(): CheckoutSelectorFactory {
         }
     );
 
-    const getGrandTotal = createSelector(
+    const getOutstandingBalance = createSelector(
         getCheckout,
         getCheckout => (useStoreCredit?: boolean) => {
             const checkout = getCheckout();
@@ -82,7 +82,7 @@ export function createCheckoutSelectorFactory(): CheckoutSelectorFactory {
             const grandTotal = checkout.grandTotal || 0;
             const storeCredit = checkout.customer.storeCredit || 0;
 
-            return useStoreCredit ? Math.max(grandTotal - storeCredit, 0) : grandTotal;
+            return useStoreCredit ? Math.max(grandTotal - storeCredit, 0) : checkout.outstandingBalance;
         }
     );
 
@@ -124,7 +124,7 @@ export function createCheckoutSelectorFactory(): CheckoutSelectorFactory {
                 customer,
                 giftCertificates,
             }),
-            getGrandTotal: getGrandTotal(state, {
+            getOutstandingBalance: getOutstandingBalance(state, {
                 billingAddress,
                 cart,
                 consignments,
